60063f753545da1c8fdc11ca03908aed6f303bbc
Christian Willsea <christianwillsea@gmail.com>
2023-04-26 19:58:53 -0700

    Add preliminary query for getOne product

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -7,6 +7,8 @@ module.exports = {
     .catch((err) => {res.status(500).send('ERROR getting all product data')})
   },
   getOne: (req, res) => {
-    res.send('Getting one product in controllers')
+   models.product.getOne(req.params.product_id)
+   .then((result) => {res.status(200).send(result.rows)})
+   .catch((err) => {res.status(500).send('ERROR getting one product\'s data ')})
   }
 }
--- server/models/product.js
+++ server/models/product.js
@@ -8,7 +8,8 @@ module.exports = {
    const params = [count, offset]
    return db.query('SELECT product_id AS id, product_name AS name, slogan, product_description AS description, sdc.category.category_name AS category, default_price FROM sdc.product JOIN sdc.category ON sdc.product.category_id = sdc.category.category_id ORDER BY product_id ASC LIMIT $1 OFFSET $2;', params)
   },
-  getOne: () => {
-//
+  getOne: (product_id) => {
+    console.log('THIS IS THE ID', product_id)
+    return db.query('SELECT sdc.product.product_id AS id, product_name AS name, slogan, product_description AS description, sdc.category.category_name AS category, default_price, feature, feature_value AS value FROM sdc.product JOIN sdc.category ON sdc.product.category_id = sdc.category.category_id JOIN sdc.product_features ON sdc.product.product_id = sdc.product_features.product_id WHERE sdc.product.product_id = $1;',[product_id])
   }
 }
