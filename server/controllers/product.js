04f75bdb15a0306fad8edf46130eae0dbb0acdb8
Christian Willsea <christianwillsea@gmail.com>
2023-04-26 19:35:50 -0700

    Made sure just responded to client with rows in product

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -3,7 +3,7 @@ const models = require('../models');
 module.exports = {
   getAll: (req, res) => {
     models.product.getAll(req.query)
-    .then((result) => {res.status(200).send(result)})
+    .then((result) => {res.status(200).send(result.rows)})
     .catch((err) => {res.status(500).send('ERROR getting all product data')})
   },
   getOne: (req, res) => {
