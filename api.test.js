6c018d55178328a9d90c107822f6e2b519ea2024
Christian Willsea <christianwillsea@gmail.com>
2023-05-04 18:54:28 -0700

    Complete all products inital tests, start specific product tests

--- api.test.js
+++ api.test.js
@@ -10,23 +10,34 @@ describe('GET /products', () => {
   })
   test('return all products with the expected properties', async() => {
     const response = await request(baseURL).get("/products");
-    expect(response.body[0]).toHaveProperty('id')
-    expect(response.body[0]).toHaveProperty('name')
-
+    expect(response.body[0]).toHaveProperty('id');
+    expect(response.body[0]).toHaveProperty('name');
+    expect(response.body[0]).toHaveProperty('slogan');
+    expect(response.body[0]).toHaveProperty('description');
+    expect(response.body[0]).toHaveProperty('category');
+    expect(response.body[0]).toHaveProperty('default_price');
   })
 })
 
 
-// //router.get('/products/:product_id', controllers.product.getOne);
-// describe('GET /products/:product_id', () => {
-//   test('succesfully return a 200 response', async() => {
-//     const response = await request(baseURL).get("/products/1");
-//     expect(response.statusCode).toBe(200);
-//   })
-//   test('return the data in the expected format', () => {
-//     expect(sum(1,2)).toBe(3);
-//   })
-// })
+//router.get('/products/:product_id', controllers.product.getOne);
+describe('GET /products/:product_id', () => {
+  test('succesfully return a 200 response', async() => {
+    const response = await request(baseURL).get("/products/1");
+    expect(response.statusCode).toBe(200);
+  })
+  test('return all products with the expected properties', async() => {
+    const response = await request(baseURL).get("/products/1");
+    expect(response.body[0]).toHaveProperty('id');
+    expect(response.body[0]).toHaveProperty('name');
+    expect(response.body[0]).toHaveProperty('slogan');
+    expect(response.body[0]).toHaveProperty('description');
+    expect(response.body[0]).toHaveProperty('category');
+    expect(response.body[0]).toHaveProperty('default_price');
+    expect(response.body[0]).toHaveProperty('features');
+
+  })
+})
 // //router.get('/products/:product_id/styles', controllers.styles.get);
 // describe('GET /products/:product_id/styles', () => {
 //   test('succesfully return a 200 response', async() => {
