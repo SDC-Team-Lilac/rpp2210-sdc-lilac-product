71466f2f401dcca8be7fab3b3a05daad42ef17e6
Christian Willsea <christianwillsea@gmail.com>
2023-05-04 18:56:21 -0700

    Add default price to getOne product model

--- server/models/product.js
+++ server/models/product.js
@@ -9,6 +9,6 @@ module.exports = {
    return db.query('SELECT product_id AS id, product_name AS name, slogan, product_description AS description, sdc.category.category_name AS category, default_price FROM sdc.product JOIN sdc.category ON sdc.product.category_id = sdc.category.category_id ORDER BY product_id ASC LIMIT $1 OFFSET $2;', params)
   },
   getOne: (product_id) => {
-    return db.query("SELECT sdc.product.product_id AS id, product_name AS name, slogan, product_description AS description, sdc.category.category_name AS category, JSON_AGG(JSON_BUILD_OBJECT('feature', feature, 'value', feature_value)) AS features FROM sdc.product JOIN sdc.category ON sdc.product.category_id = sdc.category.category_id INNER JOIN sdc.product_features USING (product_id) WHERE sdc.product.product_id = $1 GROUP BY sdc.product.product_id, sdc.category.category_id;", [product_id])
+    return db.query("SELECT sdc.product.product_id AS id, product_name AS name, slogan, product_description AS description, sdc.category.category_name AS category, default_price, JSON_AGG(JSON_BUILD_OBJECT('feature', feature, 'value', feature_value)) AS features FROM sdc.product JOIN sdc.category ON sdc.product.category_id = sdc.category.category_id INNER JOIN sdc.product_features USING (product_id) WHERE sdc.product.product_id = $1 GROUP BY sdc.product.product_id, sdc.category.category_id;", [product_id])
   }
 }
